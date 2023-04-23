b88c46d9bd8a93e4bea594e13fcb55a99411a671
Leandro <leandrojuarez655@gmail.com>
2023-04-22 21:26:15 -0400

    Test: New Api GET

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,9 @@
-
+import { supabase } from "../../../lib/supabaseClient";
 export default async function handler(req, res) {
-  if(req.method == 'GET'){
-    console.log({data: users})
+  const { data, error } = await supabase.from('users').select('Name')
+  if(error){
+    return res.status(500).json({error: error.message})
   }
-}
+  res.status(200).json({users:data})
+}  
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,22 @@
 import { useState } from "react";
-import { supabase } from "/lib/supabaseClient";
-
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
   const [error, setError] = useState(null);
-
-  const fetchUsers = async () => {
-    try {
-      const { data } = await supabase.from("users").select("Name");
-      const userNames = data.map((user) => user.Name);
-      setUsers(userNames);
+  const handleClick = async () => {
+    try{
+      const response = await fetch('/api/hello');
+      const { users } = await response.json();
+      setUser(users);
       setError(null);
-    } catch (error) {
-      console.log(error);
-      setError("Error fetching users.");
+    }catch(error){
+      setError('Error fetching users.')
     }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    await fetchUsers();
   };
 
   return (
     <div>
-      <h1>Aprendiendo Next.js desde Cero</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <button type="submit">Fetch Users</button>
-        </label>
-      </form>
-      {error && <div>Error: {error}</div>}
-      <div>
-        <h2>User Names:</h2>
-        <ul>
-          {users.map((user) => (
-            <li key={user}>{user}</li>
-          ))}
-        </ul>
-      </div>
+      <button onClick={handleClick}>Obtener nombre de Usuarios</button>
+      <p>Nombre de la tabla: {user.map((eachUser) => (eachUser.Name))}</p>
     </div>
   );
 }
-
